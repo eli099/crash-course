d5acabcf29888b5c385304e17c14f47527fdbe73
Eliott McKenzie <e4991m@gmail.com>
2024-12-12 16:21:40 +0000

    learnt linking, added links

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header>
       <h3>Header</h3>
+      <Link href="/">Homepage</Link>
+      <Link href="/blog">Blog</Link>
     </header>
   );
 }
